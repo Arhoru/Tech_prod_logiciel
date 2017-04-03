71f6c4dcb0cbaf1323b7dd87c2d8e0149f7ea55e
Arthur TAMPON <E16A133U@U-INF-J-F222-05.iut-nantes.univ-nantes.prive>
2017-04-03 11:43:51 +0200

    test ajouter pour s'assurer de l'unicité des clé primaires

--- src/envoi/recuperation/recuperation_installations.py
+++ src/envoi/recuperation/recuperation_installations.py
@@ -6,5 +6,7 @@ def recuperer():
         next(spamreader)
         liste_installations = []
         for row in spamreader:
-            liste_installations.append([row[1], row[0], row[6] + ' ' + row[7], row[4], row[2]])
+            tmp = [row[1], row[0], row[6] + ' ' + row[7], row[4], row[2]]
+            if tmp not in liste_activites:
+                liste_activites.append(tmp)
         return liste_installations
---
+++ src/import_csv.test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from envoi import envoi_installations
+from envoi import envoi_equipements
+from envoi import envoi_activites
+from envoi import envoi_equipements_activites
+
+class EnvoiTest(unittest.TestCase):
+
+    def test
+
+unittest.main()
